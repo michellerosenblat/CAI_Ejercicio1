d06ac9e63e6835544f1259f8221cbb5f3ad71f99
michellerosenblat <70615029+michellerosenblat@users.noreply.github.com>
2020-09-02 20:46:02 -0300

    Armado de For
    Primeros cambios.
    Armado de for con condiciones.

---
+++ CAI_Ejercicio1/CAI_Ejercicio1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i<=593; i++)
+            {
+                if (i%3 == 0)
+                {
+                    if (i%5 == 0 )
+                    {
+                        Console.WriteLine(i + "- FooBar");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i + "- Foo");
+                    }
+                    
+                }
+                else
+                {
+                    if (i%5== 0)
+                    {
+                        Console.WriteLine(i + "- Bar");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
